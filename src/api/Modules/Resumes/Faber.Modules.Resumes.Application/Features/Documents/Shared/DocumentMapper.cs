3bbaaa8ed1aa8c36034cc590631d4170198ea585
Viski <umi0n3@gmail.com>
2026-09-19 16:25:31 +0200

    refactor(resumes): drop the unused document mapper

--- src/api/Modules/Resumes/Faber.Modules.Resumes.Application/Features/Documents/Shared/DocumentMapper.cs
+++
@@ -1,9 +0,0 @@
-namespace Faber.Modules.Resumes.Application.Features.Documents.Shared;
-
-public static class DocumentMapper
-{
-    public static DocumentCommand MapToCommand(this DocumentRequest request, Guid userId)
-    {
-        return new DocumentCommand(request.ResumeId, userId);
-    }
-}
